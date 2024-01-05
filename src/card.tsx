3c6e1202adc400ac212043a8033d0a333a508911
Tony <legendmastertony@gmail.com>
2024-01-05 19:33:54 +0800

    Polish

--- src/card.tsx
+++ src/card.tsx
@@ -1,16 +1,54 @@
-import { createSignal, onCleanup, onMount } from 'solid-js'
+import { createEffect, createSignal, onCleanup, onMount } from 'solid-js'
 import { createStore } from 'solid-js/store'
 
 import './card.css'
 
 export function Card() {
-	let container: HTMLDivElement
+	let card: HTMLDivElement
 	let overlay: HTMLDivElement
 
 	const [open, setOpen] = createSignal(false)
+	const [cardRect, setCardRect] = createStore({
+		top: '0px',
+		left: '0px',
+		width: '100%',
+		height: '100%',
+	})
+
+	function updateCardRect() {
+		const { top, left, width, height } = card.getBoundingClientRect()
+		setCardRect({
+			top: `${top}px`,
+			left: `${left}px`,
+			width: `${width}px`,
+			height: `${height}px`,
+		})
+	}
+	onMount(() => {
+		updateCardRect()
+	})
+
+	function enableBodyScrolling(enable: boolean) {
+		if (enable) {
+			document.body.style.paddingRight = ''
+			document.body.style.overflow = ''
+		} else {
+			document.body.style.paddingRight = `${
+				window.innerWidth - document.documentElement.clientWidth
+			}px`
+			document.body.style.overflow = 'hidden'
+		}
+	}
+
+	function changeOpen(state: boolean) {
+		updateCardRect()
+		enableBodyScrolling(!state)
+		setOpen(state)
+	}
+
 	function closeOnEsc(event: KeyboardEvent) {
 		if (event.key === 'Escape') {
-			setOpen(false)
+			changeOpen(false)
 		}
 	}
 	onMount(() => {
@@ -19,31 +57,6 @@ export function Card() {
 	onCleanup(() => {
 		window.removeEventListener('keydown', closeOnEsc)
 	})
-
-	const [transitioning, setTransitioning] = createSignal(false)
-	const [originalPositions, setOriginalPositions] = createStore({
-		top: '0px',
-		left: '0px',
-		width: '0px',
-		height: '0px',
-	})
-
-	function changeOpen(state: boolean) {
-		const { top, left, width, height } = container.getBoundingClientRect()
-		setOriginalPositions({
-			top: `${top}px`,
-			left: `${left}px`,
-			width: `${width}px`,
-			height: `${height}px`,
-		})
-		setTransitioning(true)
-		requestAnimationFrame(() => {
-			requestAnimationFrame(() => {
-				setTransitioning(false)
-			})
-		})
-		setOpen(state)
-	}
 
 	let [description, setDescription] = createSignal('')
 
@@ -70,6 +83,7 @@ export function Card() {
 	return (
 		<div
 			class="card"
+			tabindex="0"
 			onClick={(ev) => {
 				if (open()) {
 					if (ev.target === overlay) {
@@ -79,29 +93,45 @@ export function Card() {
 					changeOpen(true)
 				}
 			}}
-			classList={{ open: open(), transitioning: transitioning() }}
-			style={{
-				'--original-top': originalPositions.top,
-				'--original-left': originalPositions.left,
-				'--original-width': originalPositions.width,
-				'--original-height': originalPositions.height,
+			onKeyDown={(ev) => {
+				if (ev.key === ' ') {
+					changeOpen(!open())
+					ev.preventDefault()
+				}
 			}}
+			classList={{ open: open() }}
+			style={{
+				'--card-top': cardRect.top,
+				'--card-left': cardRect.left,
+				'--card-width': cardRect.width,
+				'--card-height': cardRect.height,
+			}}
+			ref={card!}
 		>
 			<div class="overlay" ref={overlay!}></div>
-			<div class="fake-container"></div>
-			<div class="container" ref={container!}>
-				<div class="title">
-					<div class="icon"></div>
-					<div class="text">Something Important</div>
-				</div>
-				<div
-					class="description"
-					onTransitionStart={() => {
-						setDescription('')
-					}}
-					onTransitionEnd={updateDescription}
-				>
-					{description()}
+			<div class="container-decor">
+				<div class="container">
+					<div class="title">
+						<div class="icon"></div>
+						<div class="text">Some Project</div>
+					</div>
+					<div class="description">
+						{
+							// description()
+							open()
+								? `Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?`
+								: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio blanditiis voluptates quod esse veritatis est porro doloribus cumque voluptatum nostrum! Explicabo praesentium aspernatur recusandae molestiae harum sed consequatur quam ullam?`
+						}
+					</div>
 				</div>
 			</div>
 		</div>
